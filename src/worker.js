9f8b09195b5ac3945c24ee26c092d47a0a88b11d
Wendy <wendycarvajale>
2023-10-09 22:36:51 -0400

    Server workess

--- src/worker.js
+++ src/worker.js
@@ -3,7 +3,7 @@ console.log('Service Worker Works');
 self.addEventListener('push', e => {
     const data = e.data.json();
     console.log(data)
-    console.log('Notification Received');
+    console.log('Notification recibida');
     self.registration.showNotification(data.title, {
         body: data.message,
         icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1024px-Archlinux-icon-crystal-64.svg.png'
